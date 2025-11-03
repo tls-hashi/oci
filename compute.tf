4da12a9862abbf84c8cf0caed3982ef94c455350
Tommy <tommy.speigner@hashicorp.com>
2025-11-03 14:15:06 -0800

    Switch to E2.1.Micro for guaranteed capacity

--- compute.tf
+++ compute.tf
@@ -1,20 +1,25 @@
+# Alternative compute configuration using E2.1.Micro (x86 Free Tier)
+# Rename to compute.tf to use this instead of A1.Flex
+
+# NOTE: E2.1.Micro can ONLY be created in AD-3 (emiq:PHX-AD-3)
+
 # Availability domains
 data "oci_identity_availability_domains" "ads" {
   compartment_id = local.compartment_ocid
 }
 
-# Latest Ubuntu 22.04 ARM image for A1.Flex
-data "oci_core_images" "ubuntu_arm" {
+# Latest Ubuntu 22.04 x86 image for E2.1.Micro
+data "oci_core_images" "ubuntu_x86" {
   compartment_id           = local.compartment_ocid
   operating_system         = "Canonical Ubuntu"
   operating_system_version = "22.04"
-  shape                    = "VM.Standard.A1.Flex"
+  shape                    = "VM.Standard.E2.1.Micro"
   sort_by                  = "TIMECREATED"
   sort_order               = "DESC"
   
   filter {
     name   = "display_name"
-    values = ["^Canonical-Ubuntu-22.04-aarch64.*"]
+    values = ["^Canonical-Ubuntu-22.04-\\d{4}\\.\\d{2}\\.\\d{2}-\\d+$"]
     regex  = true
   }
 }
@@ -32,25 +37,22 @@ locals {
     runcmd:
       - systemctl enable apache2
       - systemctl start apache2
-      - echo '<h1>Welcome to OCI</h1><p>Deployed via HCP Terraform + Vault</p>' > /var/www/html/index.html
+      - echo '<h1>Welcome to OCI</h1><p>Deployed via HCP Terraform + Vault (x86)</p>' > /var/www/html/index.html
   EOF
 }
 
-# Compute instance
+# Compute instance - E2.1.Micro (x86, must be in AD-3)
 resource "oci_core_instance" "main" {
   compartment_id      = local.compartment_ocid
-  availability_domain = data.oci_identity_availability_domains.ads.availability_domains[1].name
+  availability_domain = data.oci_identity_availability_domains.ads.availability_domains[2].name  # MUST be index 2 (AD-3)
   display_name        = "ubuntu-instance"
-  shape               = "VM.Standard.A1.Flex"
+  shape               = "VM.Standard.E2.1.Micro"
   
-  shape_config {
-    ocpus         = 1
-    memory_in_gbs = 6
-  }
+  # No shape_config needed for Micro instances - fixed size
   
   source_details {
     source_type             = "image"
-    source_id               = data.oci_core_images.ubuntu_arm.images[0].id
+    source_id               = data.oci_core_images.ubuntu_x86.images[0].id
     boot_volume_size_in_gbs = 50
   }
   
